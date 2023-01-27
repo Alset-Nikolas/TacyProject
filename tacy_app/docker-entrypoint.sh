846ce9c4053a377e8279643d1dc2b5e11367db1d
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-01-28 00:14:57 +0300

    fix

--- tacy_app/docker-entrypoint.sh
+++ tacy_app/docker-entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Collect static files
 echo "Collect static files"
-python manage.py collectstatic --noinput
+python tacy_app/manage.py collectstatic --noinput
 
-# Apply database migrations
 echo "Apply database makemigrations"
 python tacy_app/manage.py makemigrations
 
-# Apply database migrations
 echo "Apply database migrations"
 python tacy_app/manage.py migrate
 
 echo "Create admin"
-python tacy_app/manage.py createsuperuser --email=$DJANGO_SUPERUSER_EMAIL --noinput
+python tacy_app/manage.py createsuperuser --email=$DJANGO_SUPERUSER_EMAIL --noinput || echo "admin already created"
 
 echo "start server"
-python tacy_app/manage.py runserver 0.0.0.0:8000
-# python manage.py runserver
+gunicorn tacy_app.wsgi --bind 0.0.0.0:8000 --workers 3 --threads 2
+
+exec "$@"
